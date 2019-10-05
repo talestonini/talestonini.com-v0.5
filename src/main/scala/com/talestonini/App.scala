8d8e82563edd06ce640a4f18f9474b75dbba3d17
Tales Tonini <talestonini@gmail.com>
2019-10-05 12:29:10 +1000

    building topnav

--- src/main/scala/com/talestonini/App.scala
+++ src/main/scala/com/talestonini/App.scala
@@ -27,39 +27,40 @@ object App {
     </td>
 
   @dom
+  def logoDiv =
+    <div class="logo">
+      <table>
+        <tr>
+          <td class="symbol">&#x276F;</td>
+          <td class="tales_t">T</td>
+          <td class="ales">ales</td>
+          { pronunciationTd("/tɑː \u2022 les/").bind }
+        </tr>
+        <tr>
+          <td></td>
+          <td class="tonini_t">T</td>
+          <td class="onini">onini</td>
+          { pronunciationTd("/toʊ \u2022 niː \u2022 nɪ/").bind }
+        </tr>
+        <tr>
+          <td></td>
+          <td class="dot">&#x2022;</td>
+          <td class="com">com</td>
+          <td></td>
+        </tr>
+      </table>
+    </div>
+
+  @dom
   def page: Binding[Node] = {
+    val menuItemClasses = "w3-bar-item w3-button w3-hover-none w3-border-white w3-bottombar w3-hover-border-black w3-padding-16"
     <div class="page">
       <div class="header">
-        <div class="logo">
-          <div>
-            <table>
-              <tr>
-                <td class="symbol">&#x276F;</td>
-                <td class="tales_t">T</td>
-                <td class="ales">ales</td>
-                { pronunciationTd("/tɑː \u2022 les/").bind }
-              </tr>
-              <tr>
-                <td></td>
-                <td class="tonini_t">T</td>
-                <td class="onini">onini</td>
-                { pronunciationTd("/toʊ \u2022 niː \u2022 nɪ/").bind }
-              </tr>
-              <tr>
-                <td></td>
-                <td class="dot">&#x2022;</td>
-                <td class="com">com</td>
-                <td></td>
-              </tr>
-            </table>
-          </div>
-        </div>
-        <div class="topnav">
-          <a href="#">Link</a>
-          <a href="#">Link</a>
-          <a href="#">Link</a>
-          <a href="#">Link</a>
-          <a href="#">Link</a>
+        {logoDiv.bind}
+        <div class="w3-bar topnav">
+          <a href="#" class={menuItemClasses}>Link 1</a>
+          <a href="#" class={menuItemClasses}>Link 2</a>
+          <a href="#" class={menuItemClasses}>Link 3</a>
         </div>
       </div>
 
