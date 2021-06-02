d1be4886879a31dc967f4e3894270fed96d24fc6
Tales Tonini <talestonini@gmail.com>
2021-06-02 23:10:54 +1000

    Updated the About page

--- src/main/resources/pages/About.md
+++ src/main/resources/pages/About.md
@@ -1,10 +1,27 @@
 {%
   class.name = About
 %}
-#About Tales Tonini
-Tales is a *Senior Software Engineer* working in Data Engineering at [Eliiza](https://eliiza.com.au/), in Melbourne, Australia.
+#About me
 
-#About TalesTonini.com
-This is a website created with the following technologies:
-- Scala 2.13
-- ScalaJS
+<div class="aside">
+  <table style="width:100%">
+    <tr>
+      <td style="padding-right: 15px"><img src="/img/talestonini.jpg" /></td>
+      <td>I'm <strong>Tales Tonini</strong>, software engineer working in data engineering at <a href="https://eliiza.com.au/">Eliiza</a>. My current interests in the field are Functional Programming, Distributed Systems, the Scala language, Spark and Kafka.</td>
+    </tr>
+    <tr>
+      <td colspan="2">I currently live in Melbourne, Australia, but am originally from Brazil. Aside from programming, I like to stay with family and friends, swim, walk on the beach, play soccer, hike, watch birds, play some guitar and play with Lego.</td>
+    </tr>
+  </table>
+</div>
+
+#About the website
+I started this website to practice writing about things I do at work, general interests in Software Engineering and as a way to learn ScalaJS.
+These are some of the technologies and libraries that I build it with:
+
+- [Scala 2.13](https://www.scala-lang.org/)
+- [ScalaJS](https://www.scala-js.org/)
+- [ThoughtWorks Binding](https://github.com/ThoughtWorksInc/Binding.scala)
+- [Laika](https://planet42.github.io/Laika/)
+- [Firebase](https://firebase.google.com/)
+- [Cloud Firestore](https://firebase.google.com/firebase/cloud-firestore)
--- src/main/scala/com/talestonini/pages/sourcegen/About.scala
+++ src/main/scala/com/talestonini/pages/sourcegen/About.scala
@@ -15,13 +15,30 @@ object About {
 
   @html def apply(): Binding[Node] =
     <div>
-      <h1 id="about-tales-tonini" class="title">About Tales Tonini</h1>
-      <p>Tales is a <em>Senior Software Engineer</em> working in Data Engineering at <a href="https://eliiza.com.au/">Eliiza</a>, in Melbourne, Australia.</p>
+      <h1 id="about-me" class="title">About me</h1>
+      <div class="aside">
+        <table style="width:100%">
+          <tr>
+            <td style="padding-right: 15px"><img src="/img/talestonini.jpg"/></td>
+            <td>I&#39;m <strong>Tales Tonini</strong>, software engineer working in data engineering at <a href="https://eliiza.com.au/">Eliiza</a>. My current interests in the field are Functional Programming, Distributed Systems, the Scala language, Spark and Kafka.</td>
+          </tr>
+          <tr>
+            <td colspan="2">I currently live in Melbourne, Australia, but am originally from Brazil. Aside from programming, I like to stay with family and friends, swim, walk on the beach, play soccer, hike, watch birds, play some guitar and play with Lego.</td>
+          </tr>
+        </table>
+      </div>
       
-      <h1 id="about-talestonini-com" class="section">About TalesTonini.com</h1>
-      <p>This is a website created with the following technologies:
-      - Scala 2.13
-      - ScalaJS</p>
+      <h1 id="about-the-website" class="section">About the website</h1>
+      <p>I started this website to practice writing about things I do at work, general interests in Software Engineering and as a way to learn ScalaJS.
+      These are some of the technologies and libraries that I build it with:</p>
+      <ul>
+        <li><a href="https://www.scala-lang.org/">Scala 2.13</a></li>
+        <li><a href="https://www.scala-js.org/">ScalaJS</a></li>
+        <li><a href="https://github.com/ThoughtWorksInc/Binding.scala">ThoughtWorks Binding</a></li>
+        <li><a href="https://planet42.github.io/Laika/">Laika</a></li>
+        <li><a href="https://firebase.google.com/">Firebase</a></li>
+        <li><a href="https://firebase.google.com/firebase/cloud-firestore">Cloud Firestore</a></li>
+      </ul>
     </div>
 
 }
